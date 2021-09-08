2e5c5f4e572aee2c099857b735151b6bdd1d6fe7
Nholbrook <alivegeek@gmail.com>
2021-09-08 16:53:31 -0400

    First commit

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from Load import load
 import Customers
 
 #vars
-#loadHosts = Load.Load
-#list of customer objects
+
 masterWanList = load()
 
 for each in masterWanList:
-    print(each.name())
+    print(each.name() + " " + each.wanIP())
